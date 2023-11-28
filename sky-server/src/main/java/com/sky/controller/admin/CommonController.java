fee444737c90d4c68fb40b28dd702b0bae069a16
ybc <2309300728@qq.com>
2023-11-28 20:17:12 +0800

    苍穹外卖-文件上传

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,38 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@Api(tags = "文件上穿")
+@Slf4j
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/admin/common/upload")
+    @ApiOperation("上传文件")
+    public Result<String> upload(MultipartFile file){
+        String fileOriginalFilename = file.getOriginalFilename();
+        String string = fileOriginalFilename.substring(fileOriginalFilename.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString() + string;
+        try {
+            String filePath = aliOssUtil.upload(file.getBytes(), fileName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.info("上传失败");
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
